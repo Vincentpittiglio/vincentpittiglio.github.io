2071774105c6181505ad6c6122ff5f17c947754e
Vincentpittiglio <vincentpittiglio.design@gmail.com>
2018-10-11 23:21:35 +0200

    new

--- js/script.js
+++ js/script.js
@@ -7,14 +7,74 @@ $(document).ready(function() {
       });
       });
 
-function open_window(votre_page)
-				{
-					var hauteur_popup=800;
-					var H = (screen.height - hauteur_popup) / 2;
-					var largeur_popup=800;
-					var L = (screen.width - largeur_popup) / 2;
-					pop_up = window.open(votre_page,"Popup","status=yes,scrollbars=yes,resizable=no,height="+hauteur_popup+",width="+largeur_popup+",top="+H+",left="+L);
-				}
+$(document).ready(function(){
+    $(window).on('scroll', function () {
+    
+    var elmt   = $('.from-left, .from-right, .from-bottom');
+    var topImg = $('.from-left, .from-right, .from-bottom').offset().top;
+    var scroll = $(window).scrollTop();
+    
+        $(elmt).each(function() {
+            
+            var topImg = $(this).offset().top - 950;
+            
+                if ( topImg < scroll ) {
+                
+                $(this).css("transform", "translate(0,0)");
+                $(this).css("opacity", "1");
+                
+                };
+        });
+    });
+});
+
+// Cache selectors
+var lastId,
+    topMenu = $("#fildarianne"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href='#"+id+"']").parent().addClass("active");
+   }                   
+});
+
+
 
 
 
