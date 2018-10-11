3c27331b40af692de3ce5ba03903fa576f69b327
Vincentpittiglio <vincentpittiglio.design@gmail.com>
2018-10-11 23:26:02 +0200

    reparation

--- js/script.js
+++ js/script.js
@@ -74,7 +74,46 @@ $(window).scroll(function(){
    }                   
 });
 
+$(document).ready(function() {
+
+  $('.menu').click(function(){
+    $(this).find('.barre').toggleClass('white');
+    $('.humberger').toggleClass('isOpen');
+  });
 
 
+});
 
 
+var Menu = {
+  
+  el: {
+    ham: $('.menu'),
+    menuTop: $('.menu-top'),
+    menuMiddle: $('.menu-middle'),
+    menuBottom: $('.menu-bottom')
+  },
+  
+  init: function() {
+    Menu.bindUIactions();
+  },
+  
+  bindUIactions: function() {
+    Menu.el.ham
+        .on(
+          'click',
+        function(event) {
+        Menu.activateMenu(event);
+        event.preventDefault();
+      }
+    );
+  },
+  
+  activateMenu: function() {
+    Menu.el.menuTop.toggleClass('menu-top-click');
+    Menu.el.menuMiddle.toggleClass('menu-middle-click');
+    Menu.el.menuBottom.toggleClass('menu-bottom-click'); 
+  }
+};
+
+Menu.init();
