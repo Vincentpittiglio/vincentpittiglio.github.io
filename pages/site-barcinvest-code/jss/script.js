6657bb64cdf2c4a080d984b92e74f4b282a3a9ce
Vincentpittiglio <vincentpittiglio.design@gmail.com>
2019-09-28 00:58:04 +0200

    ifs

---
+++ pages/site-barcinvest-code/jss/script.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function() {
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+      var page = $(this).attr('href'); // Page cible
+      var speed = 750; // Durée de l'animation (en ms)
+      $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+      return false;
+      });
+      });
+
+
+$(document).ready(function() {
+
+  $('.menu').click(function(){
+    $('.humberger').toggleClass('isOpen');
+    $('.menu-top').toggleClass('menu-top-click');
+    $('.menu-bottom').toggleClass('menu-bottom-click');
+    $('.menu-middle').toggleClass('menu-middle-click');
+  });
+
+
+});
+
+
+var Menu = {
+  
+  el: {
+    ham: $('.menu'),
+    menuTop: $('.menu-top'),
+    menuMiddle: $('.menu-middle'),
+    menuBottom: $('.menu-bottom')
+  },
+  
+  init: function() {
+    Menu.bindUIactions();
+  },
+  
+  bindUIactions: function() {
+    Menu.el.ham
+        .on(
+          'click',
+        function(event) {
+        Menu.activateMenu(event);
+        event.preventDefault();
+      }
+    );
+  },
+  
+  activateMenu: function() {
+    Menu.el.menuTop.toggleClass('menu-top-click');
+    Menu.el.menuMiddle.toggleClass('menu-middle-click');
+    Menu.el.menuBottom.toggleClass('menu-bottom-click'); 
+  }
+};
+
+Menu.init();
